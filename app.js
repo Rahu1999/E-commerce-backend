043407967433c3101f199986f68d673177dd83e7
Rahu1999 <rahulrajbhar19999@gmail.com>
2022-02-27 22:14:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors =require('cors');
+const app = express();
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+app.use(cors());
+app.options('*',cors())
+
+
+// middelware
+app.use(express.json())
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
+
+// Routes...
+const categoriesRoutes = require('./routers/categories');
+const ordersRoutes = require('./routers/categories')
+const productsRoutes = require('./routers/products')
+const usersRoutes = require('./routers/users')
+
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`,categoriesRoutes);
+app.use(`${api}/orders`,ordersRoutes);
+app.use(`${api}/products`,productsRoutes);
+app.use(`${api}/users`,usersRoutes);
+
+
+
+mongoose.connect(process.env.CONNECTION_STRING,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Databse connected successfully....');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000,()=>{
+     
+    console.log('Server is Running http://localhost:3000');
+})
---
+++ routers/orders.js
@@ -0,0 +1,37 @@
+const {Order} = require('../models/order');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res) =>{
+    const orderList = await Order.find();
+
+    if(!orderList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(orderList);
+})
+
+router.post(`/`,async (req,res)=>{
+    let order = new Order({
+        orderItems: req.body.orderItems,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: totalPrice,
+        user: req.body.user,
+    })
+    order = await order.save();
+
+    if(!order){
+        return res.status(404).send('The order cannot be created..')
+    }
+    else{
+        res.send(order);
+    }
+})
+
+module.exports =router;
